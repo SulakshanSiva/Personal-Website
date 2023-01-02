a5b42b4a7ad37f01278fef1b8ea78f02455abcfa
Sulakshan <sulakshansivak@gmail.com>
2023-01-02 16:52:28 -0500

    imported bootstrap

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import '../Styles/App.css'
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+        <p className='display-4'>Hi</p>
+        <Navbar/>
     </div>
   );
 }
